29b80f6c38147d8150b09997fa26fd6e290087aa
JUANGR12192004 <jgutierrezr11@ucentral.edu.co>
2025-11-23 23:17:48 -0500

    husos actualizados

--- src/main/java/com/example/worker_registry/WorkerRegistryApplication.java
+++ src/main/java/com/example/worker_registry/WorkerRegistryApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class WorkerRegistryApplication {
     public static void main(String[] args) {
+        // Usa zona horaria fija UTC-5 (America/Bogota) para toda la app
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Bogota"));
         SpringApplication.run(WorkerRegistryApplication.class, args);
     }
 }
