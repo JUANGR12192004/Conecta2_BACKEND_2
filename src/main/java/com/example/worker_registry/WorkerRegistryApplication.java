1c765f36227f5bbda509416cbc5d09882e5125d3
JUANGR12192004 <jgutierrezr11@ucentral.edu.co>
2026-03-12 21:48:42 -0500

    UPDATE-BASE DE DATOS

--- src/main/java/com/example/worker_registry/WorkerRegistryApplication.java
+++ src/main/java/com/example/worker_registry/WorkerRegistryApplication.java
@@ -1,5 +1,4 @@
 package com.example.worker_registry;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
